b796d58795604a1e4f022a527743879916a6a4ac
Brandon Ma <brandonma@me.com>
2014-03-28 12:45:26 -0400

    working as chrome app

--- fuseDemo.js
+++ fuseDemo.js
@@ -6,32 +6,19 @@ $(function() {
         $authorCheckbox = $('#author'),
         $titleCheckbox = $('#title'),
         $caseCheckbox = $('#case'),
-        $resultTemplate = $('#template'),
 
         searchAuthors = false,
         searchTitles = true,
         isCaseSensitive = false,
-
-        pushToTemp = function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['resulttemplate'] = template({"compiler":[5,">= 2.0.0"],"main":function(depth0,helpers,partials,data) {
-  var helper, functionType="function", escapeExpression=this.escapeExpression;
-  return "<li class=\"result-item\"> \r\n "
-    + escapeExpression(((helper = helpers.title || (depth0 && depth0.title)),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
-    + " \r\n  <span>"
-    + escapeExpression(((helper = helpers.author || (depth0 && depth0.author)),(typeof helper === functionType ? helper.call(depth0, {"name":"author","hash":{},"data":data}) : helper)))
-    + "</span>\r\n</li>";
-},"useData":true});},
 
         fuse;
 
     function search() {
       var r = fuse.search($inputSearch.val());
       $result.empty();
-      $.each(r, /*function() {
+      $.each(r, function() {
         $result.append('<li class="result-item">' + this.title + ', <span>' + this.author + '</span></li>');
-      }*/
-      pushToTemp());
+      });
     }
 
     function createFuse() {
