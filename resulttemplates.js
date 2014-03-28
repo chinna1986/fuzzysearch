21cbf009212b6d17dbb8ce160ae7cc3a189cf23b
Brandon Ma <brandonma@me.com>
2014-03-28 15:46:48 -0400

    working webpage version

--- resulttemplates.js
+++ resulttemplates.js
@@ -2,9 +2,10 @@ var pushToTemp = function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['resulttemplate'] = template({"compiler":[5,">= 2.0.0"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", escapeExpression=this.escapeExpression;
-  return "<li class=\"result-item\"> \r\n	"
+  return "<ul id=\"results\">\r\n	<li class=\"result-item\"> \r\n		"
     + escapeExpression(((helper = helpers.title || (depth0 && depth0.title)),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
-    + " \r\n	<span>"
+    + " \r\n		<span>"
     + escapeExpression(((helper = helpers.author || (depth0 && depth0.author)),(typeof helper === functionType ? helper.call(depth0, {"name":"author","hash":{},"data":data}) : helper)))
-    + "</span>\r\n</li>";
-},"useData":true});};
+    + "</span>\r\n	</li>\r\n</ul>";
+},"useData":true});
+}
